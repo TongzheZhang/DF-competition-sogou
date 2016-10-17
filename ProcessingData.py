4421ee8ef3e76a3e90f29f3cbcfbd30e7b072b75
Tongzhe Zhang <richard10ztz@gmail.com>
2016-10-17 17:41:14 +0800

    test

---
+++ ProcessingData.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 16:27:03 2016
+
+@author: Richard
+"""
+
+import pandas as pd
+
+TrainingData = pd.read_excel('../data/test.xlsx')
+print TrainingData
