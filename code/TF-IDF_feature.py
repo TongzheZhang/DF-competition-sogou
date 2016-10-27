7ac88dcf6eaff9ae82e6ea0b0b28c715e0d5279d
Tongzhe Zhang <richard10ztz@gmail.com>
2016-10-27 22:48:04 +0800

    提取体征还没有调通
    正在进行去除停止词的工作

--- code/TF-IDF_feature.py
+++ code/TF-IDF_feature.py
@@ -14,28 +14,45 @@ from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
 from numpy import *
-import sys
 import string
+#reload(sys)后无法使用print，使用以下办法可以使用print
+stdout = sys.stdout
 reload(sys)
+sys.stdout = stdout
+
 sys.setdefaultencoding( "utf-8" )
 sys.path.append("F:\study\master of TJU\DF\Sogou\DF-competition-sogou\data")
 #from numpy import *
-fr = open(r'F:\study\master of TJU\DF\Sogou\DF-competition-sogou\data\age\test.txt')
+fr = open(r'F:\study\master of TJU\DF\Sogou\DF-competition-sogou\data\sex\sex1_for_test.txt')
 fr_list = fr.read()
 dataList = fr_list.split('\n')
+
+
 data = []
-for oneline in dataList:
-	data.append(" ".join(jieba.cut(oneline))) 
+
+
+
+
+
+for oneline in dataList: 
+    print 'i am running... in 1'
+    data.append(" ".join(jieba.cut(oneline)))    
+
+
 #将得到的词语转换为词频矩阵
 freWord = CountVectorizer()
 #统计每个词语的tf-idf权值
+
+print 'i am running... in 2'
 transformer = TfidfTransformer()
 #计算出tf-idf(第一个fit_transform),并将其转换为tf-idf矩阵(第二个fit_transformer)
 tfidf = transformer.fit_transform(freWord.fit_transform(data))
 #获取词袋模型中的所有词语
 word = freWord.get_feature_names()
+print 'i am running... in 3'
 #得到权重
 weight = tfidf.toarray()
+print 'i am running... in 4'
 tfidfDict = {}
 for i in range(len(weight)):
 	for j in range(len(word)):
@@ -48,6 +65,6 @@ for i in range(len(weight)):
 				tfidfDict.update({getWord:getValue})
 sorted_tfidf = sorted(tfidfDict.iteritems(),
 					  key = lambda d:d[1],reverse = True)
-fw = open(r'F:\study\master of TJU\DF\Sogou\DF-competition-sogou\data\age\resulttest.txt','w')
+fw = open(r'F:\study\master of TJU\DF\Sogou\DF-competition-sogou\data\sex\result_of_sex1_for_test.txt','w')
 for i in sorted_tfidf:
 	fw.write(i[0] + '\t' + str(i[1]) +'\n')
